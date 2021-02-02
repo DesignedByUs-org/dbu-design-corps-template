2d768b2ae2d1bee66e7af66781aa2e1d078d6843
Anna Tyan <38844391+annajt178@users.noreply.github.com>
2021-02-02 17:55:55 -0600

    Revert "Update index.js"
    This reverts commit 43fca5591f86ffb6d5ecfab0fd3538ab592a68cb.

--- src/pages/index.js
+++ src/pages/index.js
@@ -520,7 +520,6 @@ export const pageQuery = graphql`
     ) {
       totalCount
     }
-    
     tagCountFounder: allTwitterProfile(
       filter: { profile: { tags: { position: { founder: { eq: true } } } } }
     ) {
